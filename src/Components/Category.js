21f84d3c7e930c47d189e963b4e436c78ca8283c
prikshitgupta <prikshit762@gmail.com>
2022-02-25 10:50:39 +0530

    lowercase

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -5,7 +5,7 @@ export const Category = (props) => {
   return (
     <button className="btn btn-primary" onClick=
     {(e)=>props.selectCategoryHandler(e,props.category)}>
-      {props.category.toUpperCase()}
+      {props.category}
     </button>
   );
 };
